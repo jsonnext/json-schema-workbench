1bf4d473901c1be9a6d6aeead6050ebc03c0ee65
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-11-11 17:59:36 +0100

    removed Card components for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,15 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import { JSONSchemaEditor } from "@/components/editor/json-schema-editor"
 import { JSONValueEditor } from "@/components/editor/json-value-editor"
 
 export default function IndexPage() {
   return (
     <section className="grid h-[90vh] w-full grid-cols-2 gap-2 pb-8">
-      <Card>
-        <CardHeader>
-          <CardTitle>JSON Schema</CardTitle>
-          <CardDescription>
-            Specify the schema for your JSON data
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="h-full overflow-scroll">
-            <JSONSchemaEditor />
-          </div>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>JSON Value</CardTitle>
-          <CardDescription>Enter your JSON data</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="h-full overflow-scroll">
-            <JSONValueEditor />
-          </div>
-        </CardContent>
-      </Card>
+      <div id="json-schema-editor" className="h-full overflow-scroll">
+        <JSONSchemaEditor />
+      </div>
+      <div id="json-value-editor" className="h-full overflow-scroll">
+        <JSONValueEditor />
+      </div>
     </section>
   )
 }
